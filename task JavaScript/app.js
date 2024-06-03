a3a3ec7da1724254ebc850ba5e552951d392519c
Stepan250 <67107867+Stepan250@users.noreply.github.com>
2024-06-03 04:15:46 +0300

    Add files via upload

--- task JavaScript/app.js
+++ task JavaScript/app.js
@@ -21,16 +21,23 @@ function sumNumbers(operation) {
     let number1 = parseFloat(input1)
     let number2 = parseFloat(input2)
     let result
+    let operator
 
     if (operation === '+') {
         result = number1 + number2
+        operator = '+'
     } else if (operation === '-') {
         result = number1 - number2
+        operator = '-'
     } else if (operation === '*') {
         result = number1 * number2
+        operator = 'x'
     } else if (operation === '/') {
         result = number1 / number2
+        operator = '/'
     }
 
     document.getElementById('result').textContent = result
+    document.getElementById('operator').textContent= operator
+
 }
