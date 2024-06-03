0c01b41584a057ee9ce65848345c350bc19637a3
Stepan250 <67107867+Stepan250@users.noreply.github.com>
2024-06-03 03:34:35 +0300

    Add files via upload

--- task JavaScript/app.js
+++ task JavaScript/app.js
@@ -1,11 +1,36 @@
-function sumNumbers() {
-    let input1 = document.getElementById('input1').value;
-    let input2 = document.getElementById('input2').value;
+document.getElementById('plus').onclick = function() {
+    sumNumbers('+')
+}
 
-    let number1 = parseFloat(input1);
-    let number2 = parseFloat(input2);
+document.getElementById('minus').onclick = function() {
+    sumNumbers('-')
+}
 
-    let sum = number1 + number2;
+document.getElementById('multiplication').onclick = function() {
+    sumNumbers('*')
+}
 
-    document.getElementById('result').textContent = sum;
+document.getElementById('division').onclick = function() {
+    sumNumbers('/')
+}
+
+function sumNumbers(operation) {
+    let input1 = document.getElementById('input1').value
+    let input2 = document.getElementById('input2').value
+
+    let number1 = parseFloat(input1)
+    let number2 = parseFloat(input2)
+    let result
+
+    if (operation === '+') {
+        result = number1 + number2
+    } else if (operation === '-') {
+        result = number1 - number2
+    } else if (operation === '*') {
+        result = number1 * number2
+    } else if (operation === '/') {
+        result = number1 / number2
+    }
+
+    document.getElementById('result').textContent = result
 }
